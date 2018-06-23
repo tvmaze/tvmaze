d5f65d61b4e20ad5d9277306c5e5e069d0d1c5c3
Labrys of Knossos <Labrys.git@gmail.com>
2018-06-22 22:44:02 -0400

    Add hacking code quality tests
    Adds the following error codes:
    
    H101	Include your name with TODOs as in # TODO(yourname)
    H102-3	Newly contributed Source Code should be licensed under Apache 2.0
    H104	Files with no code shouldn't contain any license header nor comments,
    		and must be left completely empty.
    H105	Don't use author tags. We use version control instead.
    H106	Don't put vim configuration in source files (off by default).
    
    H201	Do not write except:, use except Exception: at the very least.
    H202	Use assertRaises instead of testing for Exception
    H203	Use assertIs(Not)None to check for None (off by default)
    H204	Use assert(Not)Equal to check for equality (off by default)
    H205	Use assert(Greater|Less)(Equal) for comparison (off by default)
    H210	Require autospec, spec, or spec_set in mock.patch() or
    		mock.patch.object() calls (off by default)
    H231	Instead of `except x,y` use `except x as y`
    H232	Octal string literals require explicit octal prefix
    H233	Use print as a function
    H234	assertEquals() is deprecated in Python 3.x, use assertEqual()
    H235	assert_() is deprecated in Python 3.x, use assertTrue() instead
    H236	Use six.add_metaclass instead of __metaclass__.
    H237	Don't use modules that were removed in Python 3
    H238	Old style classes are deprecated and no longer available in Python 3
    
    H301	Do not import more than one module per line (*)
    H303	Do not use wildcard * import (*)
    H304	Do not make relative imports
    H306	Alphabetically order your imports by the full module path.
    
    H401	Docstrings should not start with a space.
    H403	Multi line docstrings should end on a new line.
    H404	Multi line docstrings should start without a leading new line.
    H405	Multi line docstrings should start with a one line summary
    		followed by an empty line.
    
    H501	Do not use locals() or self.__dict__ for formatting strings.
    
    H702	If you have a variable to place within the string, first
    		internationalize the template string then do the replacement.
    H703	If you have multiple variables to place in the string, use keyword
    		parameters. This helps translators reorder parameters when needed.
    
    H903	Use only UNIX style newlines (\n), not Windows style (\r\n)
    H904	Delay string interpolations at logging calls (off by default).

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 
 import pathlib
 
-from setuptools import find_packages, setup
+import setuptools
 
 PROJECT_ROOT = pathlib.Path(__file__).parent
 PROJECT_NAME = 'tvmaze'
@@ -34,14 +34,14 @@ PROJECT_KEYWORDS = [
     'metadata',
 ]
 
-setup(
+setuptools.setup(
     name=PROJECT_NAME,
     version=PROJECT_VERSION,
     license=PROJECT_LICENSE,
     description=PROJECT_DESCRIPTION,
     long_description=PROJECT_README.read_text('utf-8'),
     url=PROJECT_URL,
-    packages=find_packages(str(PROJECT_SOURCE_DIRECTORY)),
+    packages=setuptools.find_packages(str(PROJECT_SOURCE_DIRECTORY)),
     package_dir={
         '': str(PROJECT_SOURCE_DIRECTORY.relative_to(PROJECT_ROOT)),
     },
